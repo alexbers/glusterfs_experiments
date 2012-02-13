0e6ff2228668511123e8ccea3b1c4b15c6ab8370
Alexander Bersenev <bay@hackerdom.ru>
2012-02-13 14:36:31 +0600

    added commands to install

---
+++ commands_to_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#./autogen
+#./configure --prefix=/usr --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --mandir=/usr/share/man --infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc --localstatedir=/var/lib --libdir=/usr/lib64 --disable-dependency-tracking --enable-fuse-client --disable-ibverbs --disable-static --enable-georeplication --disable-bdb --docdir=/usr/share/doc/glusterfs-3.2.5 --localstatedir=/var
+#make
+#make install
